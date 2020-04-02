3c1aa258c41df8afd7572c4e061da9e0afb5bffd
nicolasariza <nicolas_ariza@outlook.com>
2020-04-01 21:08:18 -0500

    Se realiza primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, Fragment } from "react";
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import Clima from "./components/Clima";
+import Error from "./components/Error";
 
 function App() {
+  const [busqueda, setBusqueda] = useState({
+    cuidad: "",
+    pais: ""
+  });
+
+  const { ciudad, pais } = busqueda;
+
+  const [consultar, setConsultar] = useState(false);
+
+  const [resultado, setResultado] = useState({});
+
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      if (consultar) {
+        const appId = "79040aa3323970bc38c240d69b812626";
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        console.log(url);
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setResultado(resultado);
+        setConsultar(false);
+        resultado.cod === "404" ? setError(true) : setError(false);
+      }
+    };
+    consultarAPI();
+  }, [consultar]);
+
+  let componente;
+
+  if (error) {
+    componente = <Error mensaje="No hay resultados" />;
+  } else {
+    componente = <Clima resultado={resultado} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Header titulo="Clima" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Formulario
+                busqueda={busqueda}
+                setBusqueda={setBusqueda}
+                setConsultar={setConsultar}
+              />
+            </div>
+            <div className="col m6 s12">{componente}</div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
---
+++ src/components/Clima.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Clima = ({ resultado }) => {
+  // Extraer los valores
+
+  const { name, main } = resultado;
+
+  if (!name) return null;
+
+  //Grados kelvin
+  const kelvin = 273.15;
+
+  return (
+    <div className="card-panel white col s12">
+      <div className="black-text">
+        <h2>
+          El clima de {name} es:{" "}
+          <p className="temperatura">
+            {parseFloat(main.temp - kelvin, 10).toFixed(2)}{" "}
+            <span> &#x2103;</span>
+          </p>
+          <p className="">
+            Temperatura máxima
+            {parseFloat(main.temp_max - kelvin, 10).toFixed(2)}{" "}
+            <span> &#x2103;</span>
+          </p>
+          <p className="">
+            Temperatura Minima:
+            {parseFloat(main.temp_min - kelvin, 10).toFixed(2)}{" "}
+            <span> &#x2103;</span>
+          </p>
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+Clima.propTypes = {
+  resultado: PropTypes.object.isRequired
+};
+
+export default Clima;
