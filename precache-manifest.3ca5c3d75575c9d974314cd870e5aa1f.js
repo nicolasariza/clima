8553e05597e19c0a54183526b82b258bda801777
nicolasariza <nicolas_ariza@outlook.com>
2020-04-01 21:18:46 -0500

    Updates

---
+++ precache-manifest.3ca5c3d75575c9d974314cd870e5aa1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9352fb8bbf7a2633937642ae755d87f",
+    "url": "/clima/index.html"
+  },
+  {
+    "revision": "7577dc7e6fb32e509128",
+    "url": "/clima/static/css/main.d7813621.chunk.css"
+  },
+  {
+    "revision": "db36a08d4bf426b1577f",
+    "url": "/clima/static/js/2.c05b4c6f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clima/static/js/2.c05b4c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7577dc7e6fb32e509128",
+    "url": "/clima/static/js/main.0814caad.chunk.js"
+  },
+  {
+    "revision": "8fadfb99865c88a5f69e",
+    "url": "/clima/static/js/runtime-main.58c53119.js"
+  }
+]);
